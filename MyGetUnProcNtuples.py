589f192e36fa33f88a40290fa54ddb9cebf3b831
Shalini <shalini.thakur@cern.ch>
2020-08-18 23:50:01 +0530

    Archived from ui

---
+++ MyGetUnProcNtuples.py
@@ -0,0 +1,68 @@
+
+#//////////////////////////////////////////////////
+#                                                 #
+# Get total Ntuples tobeprocessed form condor dir #
+# Get processed Ntuples from condor output        #
+# Get paths of the unprocessed Ntuples            #
+#                                                 #
+#//////////////////////////////////////////////////
+
+
+import os
+import sys
+import datetime
+
+
+#Sample paths at T2
+processedPaths = open("procNtuples.txt", "w")
+
+#condor output histo files
+print ""
+print"----------------------------------------------------"
+print" Fetching the un-processed ntuples, please wait ... "
+print"----------------------------------------------------"
+for line_out in open("outHistoFiles.log"):
+    line_out = line_out.strip()
+    split_line_out = line_out.split("/")
+    ntuple_out = split_line_out[-1].replace("_Anal", "")
+    #print ntuple_out
+    for line_in_all_ntuple in open("allNtuples.log"):
+        ntuple_input = line_in_all_ntuple.strip()
+	if ntuple_out in ntuple_input:
+	    processedPaths.write(ntuple_input+"\n")
+processedPaths.close()
+
+##------------------------------------------------
+# compare the two file, output the unmatched lines
+##------------------------------------------------
+def compare1(File1,File2):
+    with open(File1,'r') as f1:
+        d=set(f1.readlines())
+    with open(File2,'r') as f2:
+        e=set(f2.readlines())
+    open('unProcNtuples1.log','w').close() #Create the file
+    with open('unProcNtuples1.log','a') as f:
+        for line in list(d-e):
+           f.write(line)
+	   #print line
+
+def compare2(File1, File2):
+    with open(File1, 'r') as file1:
+        with open(File2, 'r') as file2:
+            diff = set(file1).difference(file2)
+    #same.discard('\n')
+    with open('unProcNtuples.txt', 'w') as file_out:
+        for line in diff:
+            file_out.write(line)
+
+
+#compare1("allNtuples.log", "procNtuples.txt")
+compare2("allNtuples.log", "procNtuples.txt")
+
+print ""
+print"----------------------------------------------------"
+print": 		Done       			 :"
+print": 	See unProcNtuples.txt                    :"
+print"----------------------------------------------------"
+
+
